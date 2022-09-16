2035b99e54e89d4920260060baae08f5d96acb77
João Dematte <demattejoao@gmail.com>
2022-09-16 00:11:31 -0300

    feat: add navbar animations

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,6 +2,44 @@ import Link from 'next/link';
 import { Moon, Sun } from 'phosphor-react';
 import { useEffect, useState } from 'react';
 import clsx from 'clsx';
+import { motion } from 'framer-motion';
+
+const animationVariantsContainer = {
+  hidden: {
+    opacity: 0
+  },
+  visible: {
+    opacity: 1,
+    transition: {
+      staggerChildren: 0.05
+    }
+  }
+};
+
+const animationVariantsItem = {
+  hidden: {
+    opacity: 0,
+    y: -100
+  },
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      staggerChildren: 0.05
+    }
+  }
+};
+
+const test = {
+  hidden: {
+    opacity: 0,
+    y: -100
+  },
+  visible: {
+    opacity: 1,
+    y: 0
+  }
+};
 
 const navbarLinks = [
   {
@@ -63,27 +101,38 @@ const Navbar = () => {
         }
       )}
     >
-      <div className="w-full max-w-6xl mx-auto flex justify-between items-center">
+      <motion.div
+        variants={animationVariantsContainer}
+        initial="hidden"
+        animate="visible"
+        className="w-full max-w-6xl mx-auto flex justify-between items-center"
+      >
         <Link href="/">
-          <a className="font-extrabold text-2xl dark:text-white">hexagon</a>
+          <motion.a variants={animationVariantsItem} className="font-extrabold text-2xl dark:text-white">
+            hexagon
+          </motion.a>
         </Link>
-        <ul className="flex items-center gap-12">
+        <motion.ul variants={animationVariantsItem} className="flex items-center gap-12">
           {navbarLinks.map((link) => (
             <Link href={link.path} key={link.id} passHref>
-              <a className="font-semibold text-gray-500 transition-colors duration-300 hover:text-black dark:text-gray-400 focus:text-black dark:hover:text-white dark:focus:text-white">
+              <motion.a
+                variants={test}
+                className="font-semibold text-gray-500 transition-colors duration-300 hover:text-black dark:text-gray-400 focus:text-black dark:hover:text-white dark:focus:text-white"
+              >
                 {link.name}
-              </a>
+              </motion.a>
             </Link>
           ))}
-        </ul>
-        <button
+        </motion.ul>
+        <motion.button
+          variants={animationVariantsItem}
           type="button"
           onClick={() => handleChangeTheme(theme)}
           className="focus-ring hover-ring rounded-md text-black dark:text-white"
         >
           {theme === 'light' ? <Moon size={24} weight="fill" /> : <Sun size={24} weight="fill" />}
-        </button>
-      </div>
+        </motion.button>
+      </motion.div>
     </nav>
   );
 };
