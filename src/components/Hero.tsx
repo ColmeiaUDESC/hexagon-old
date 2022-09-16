da12c5351341041ac238357186602c219ca53502
João Dematte <demattejoao@gmail.com>
2022-09-16 00:04:40 -0300

    feat: hero animations

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,14 +1,39 @@
+import { motion } from 'framer-motion';
+
+const animationVariantsTextContainer = {
+  hidden: {
+    opacity: 0
+  },
+  visible: {
+    opacity: 1,
+    transition: {
+      staggerChildren: 0.05
+    }
+  }
+};
+
+const animationVariantsTextItem = {
+  hidden: {
+    opacity: 0,
+    x: -100
+  },
+  visible: {
+    opacity: 1,
+    x: 0
+  }
+};
+
 const Hero = () => (
   <div className="w-full max-w-6xl mx-auto h-screen grid grid-cols-2 content-center">
-    <div className="block">
-      <h1 className="font-extrabold text-6xl text-black dark:text-white">
+    <motion.div variants={animationVariantsTextContainer} initial="hidden" animate="visible" className="block">
+      <motion.h1 variants={animationVariantsTextItem} className="font-extrabold text-6xl text-black dark:text-white">
         Grupo de Extensão em Software e Hardware Livre
-      </h1>
-      <p className="mt-8 text-xl text-black dark:text-white">
+      </motion.h1>
+      <motion.p variants={animationVariantsTextItem} className="mt-8 text-xl text-black dark:text-white">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio unde sint veniam magni obcaecati quas rerum.
         Repellat suscipit ut a nostrum eius eos pariatur odio consequuntur. Similique iste voluptas deserunt!
-      </p>
-      <div className="mt-8 flex gap-4">
+      </motion.p>
+      <motion.div variants={animationVariantsTextItem} className="mt-8 flex gap-4">
         <button
           type="button"
           className="px-8 py-6 text-xl bg-gray-800 rounded-md text-white font-bold focus-ring hover-ring"
@@ -22,9 +47,20 @@ const Hero = () => (
         >
           Conheça mais
         </button>
-      </div>
-    </div>
-    <img src="/illustration.svg" alt="" />
+      </motion.div>
+    </motion.div>
+    <motion.img
+      initial={{
+        opacity: 0,
+        y: -100
+      }}
+      animate={{
+        opacity: 1,
+        y: 0
+      }}
+      src="/illustration.svg"
+      alt=""
+    />
   </div>
 );
 
