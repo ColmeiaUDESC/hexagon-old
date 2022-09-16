ffc4b1266133722cfa968073b9aebdaf25dccd52
João Dematte <demattejoao@gmail.com>
2022-09-15 21:58:48 -0300

    feat: animations

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 import { Moon, Sun } from 'phosphor-react';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import clsx from 'clsx';
 
 const navbarLinks = [
   {
@@ -27,6 +28,12 @@ const navbarLinks = [
 
 const Navbar = () => {
   const [theme, setTheme] = useState<'light' | 'dark'>('light');
+  const [scrollPosition, setScrollPosition] = useState<number>(0);
+
+  const handleScroll = () => {
+    const position = window.pageYOffset;
+    setScrollPosition(position);
+  };
 
   const handleChangeTheme = (currentTheme: 'light' | 'dark') => {
     if (currentTheme === 'light') {
@@ -38,27 +45,45 @@ const Navbar = () => {
     }
   };
 
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll, { passive: true });
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <nav className="w-full max-w-6xl mx-auto px-4 py-12 flex justify-between items-center">
-      <Link href="/">
-        <a className="font-extrabold text-2xl dark:text-white">hexagon</a>
-      </Link>
-      <ul className="flex items-center gap-12">
-        {navbarLinks.map((link) => (
-          <Link href={link.path} key={link.id} passHref>
-            <a className="font-semibold text-gray-500 transition-colors duration-300 hover:text-black focus:text-black dark:hover:text-white dark:focus:text-white">
-              {link.name}
-            </a>
-          </Link>
-        ))}
-      </ul>
-      <button
-        type="button"
-        onClick={() => handleChangeTheme(theme)}
-        className="focus-ring hover-ring rounded-md text-black dark:text-white"
-      >
-        {theme === 'light' ? <Moon size={24} weight="fill" /> : <Sun size={24} weight="fill" />}
-      </button>
+    <nav
+      className={clsx(
+        'fixed top-0 left-0 w-full flex justify-between transition-all border-gray-100 dark:border-gray-800 duration-300 items-center z-50 bg-opacity-80 backdrop-blur bg-white dark:bg-gray-900 dark:bg-opacity-80',
+        {
+          'px-4 py-8 border-b': scrollPosition > 100,
+          'px-8 py-12': scrollPosition <= 100
+        }
+      )}
+    >
+      <div className="w-full max-w-6xl mx-auto flex justify-between items-center">
+        <Link href="/">
+          <a className="font-extrabold text-2xl dark:text-white">hexagon</a>
+        </Link>
+        <ul className="flex items-center gap-12">
+          {navbarLinks.map((link) => (
+            <Link href={link.path} key={link.id} passHref>
+              <a className="font-semibold text-gray-500 transition-colors duration-300 hover:text-black dark:text-gray-400 focus:text-black dark:hover:text-white dark:focus:text-white">
+                {link.name}
+              </a>
+            </Link>
+          ))}
+        </ul>
+        <button
+          type="button"
+          onClick={() => handleChangeTheme(theme)}
+          className="focus-ring hover-ring rounded-md text-black dark:text-white"
+        >
+          {theme === 'light' ? <Moon size={24} weight="fill" /> : <Sun size={24} weight="fill" />}
+        </button>
+      </div>
     </nav>
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,9 +18,37 @@ const animationVariants = {
 };
 
 const Home: NextPage = () => (
-  <div className="w-full h-screen dark:bg-gray-900">
-    <Navbar />
-    <div className="max-w-6xl w-full mx-auto">
+  <>
+    <div className="w-full h-screen">
+      <Navbar />
+      <div className="w-full max-w-6xl mx-auto h-screen grid grid-cols-2 content-center">
+        <div className="block">
+          <h1 className="font-extrabold text-6xl text-black dark:text-white">
+            Grupo de Extensão em Software e Hardware Livre
+          </h1>
+          <p className="mt-8 text-xl text-black dark:text-white">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio unde sint veniam magni obcaecati quas rerum.
+            Repellat suscipit ut a nostrum eius eos pariatur odio consequuntur. Similique iste voluptas deserunt!
+          </p>
+          <div className="mt-8 flex gap-4">
+            <button
+              type="button"
+              className="px-8 py-6 text-xl bg-gray-800 rounded-md text-white font-bold focus-ring hover-ring"
+            >
+              Entre em contato
+            </button>
+
+            <button
+              type="button"
+              className="px-8 py-6 text-xl rounded-md text-black font-bold focus-ring hover-ring dark:text-white"
+            >
+              Conheça mais
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div className="mt-8 max-w-6xl w-full mx-auto">
       <div className="w-full flex flex-col gap-4">
         <Link href="/" passHref>
           <a className="flex items-center gap-2 self-end font-medium text-gray-500 hover:text-black dark:text-gray-500 dark:hover:text-white transition-colors duration-300">
@@ -28,7 +56,13 @@ const Home: NextPage = () => (
             <ArrowRight size={20} weight="regular" />
           </a>
         </Link>
-        <motion.div variants={animationVariants} initial="hidden" animate="show" className="grid grid-cols-3 gap-8">
+        <motion.div
+          variants={animationVariants}
+          initial="hidden"
+          whileInView="show"
+          viewport={{ once: true }}
+          className="grid grid-cols-3 gap-8"
+        >
           <Article
             title="Desenvolvendo hexagon"
             date={new Date()}
@@ -50,7 +84,7 @@ const Home: NextPage = () => (
         </motion.div>
       </div>
     </div>
-  </div>
+  </>
 );
 
 export default Home;
